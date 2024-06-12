50f6b23db40fda5473d8f9f71d85ba45922f1a33
Hashim Ali <hashim_ali94@outlook.com>
2024-06-12 11:20:04 -0400

    training RawGat on laundered database

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 ################ set these parameters ###############
 
 # database type
-db_type = 'asvspoof_eval_laundered' # asvspoof_eval_laundered or in_the_wild or asvspoof_train or asvspoof_train_laundered
+db_type = 'asvspoof_train_laundered' # asvspoof_eval_laundered or in_the_wild or asvspoof_train or asvspoof_train_laundered
 
 # path to the database
-# db_folder = '/data/Data/ASVSpoofData_2019_train_laundered'
+db_folder = '/data/Data/ASVSpoofData_2019_train_laundered'
 # db_folder = '/data/Data/ds_wild/'
-db_folder = '/data/Data/ASVSpoofLaunderedDatabase/ASVSpoofLaunderedDatabase'
+# db_folder = '/data/Data/ASVSpoofLaunderedDatabase/ASVSpoofLaunderedDatabase'
 
 # only used for training
 data_names = ['ASVspoof2019_LA_train', 'ASVspoof2019_LA_dev', 'ASVSpoofData_2019_train_10_percent_laundered']
@@ -16,11 +16,11 @@ data_names = ['ASVspoof2019_LA_train', 'ASVspoof2019_LA_dev', 'ASVSpoofData_2019
 # laundering type and laundering parameter, 
 # look at the readme file of the database for laundering parameters for each laundering type
 
-laundering_type = 'Reverberation' # Recompression, Reverberation, Filtering, Resampling, train_laundered for laundered training, wild for in the wild
-laundering_param = 'RT_0_9'         # random for laundered traning, wild for in the wild
+# laundering_type = 'Reverberation' # Recompression, Reverberation, Filtering, Resampling, train_laundered for laundered training, wild for in the wild
+# laundering_param = 'RT_0_9'         # random for laundered traning, wild for in the wild
 
-# laundering_type = 'train_laundered' # Recompression, Reverberation, Filtering, Resampling, train_laundered for laundered training, wild for in the wild
-# laundering_param = 'random'         # random for laundered traning, wild for in the wild
+laundering_type = 'train_laundered' # Recompression, Reverberation, Filtering, Resampling, train_laundered for laundered training, wild for in the wild
+laundering_param = 'random'         # random for laundered traning, wild for in the wild
 
 # feature output directory
 feat_dir = '/data/Features/'
@@ -30,8 +30,8 @@ feature_type = 'cqcc'
 feature_format = 'h5f' # h5f or pkl
 
 audio_ext = '.flac'
-# data_types = ['train', 'dev', 'train']
-data_types = ['eval']
+data_types = ['train', 'dev', 'train']
+# data_types = ['eval']
 data_labels = ['bonafide', 'spoof']
 
 # model parameters
@@ -39,10 +39,11 @@ n_comp = 512
 
 # score dir
 score_dir = '../Score_Files_laundered_train'
+# score_dir = '../Score_Files'
 
 ############# no need to set this parameter unless the name of the database is changed #############
 # if db_type == 'in_the_wild':
 # protocol_filenames = ['wild_meta.txt']
-# protocol_filenames = ['ASVspoof2019.LA.cm.train.trn.txt', 'ASVspoof2019.LA.cm.dev.trl.txt', 'ASVSpoofData_2019_train_10_percent_laundered_protocol.txt']
+protocol_filenames = ['ASVspoof2019.LA.cm.train.trn.txt', 'ASVspoof2019.LA.cm.dev.trl.txt', 'ASVSpoofData_2019_train_10_percent_laundered_protocol.txt']
 # else:
-protocol_filenames = ['ASVspoofLauneredDatabase_' + laundering_type + '.txt']
+# protocol_filenames = ['ASVspoofLauneredDatabase_' + laundering_type + '.txt']
